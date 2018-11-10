4cc690150881c66d40ad739676c9680015b9e141
1brenocg <brenocg@alu.ufc.br>
2018-11-09 22:00:33 -0300

    adicionando conector polimorfico, salvando dados de conexao ao banco

--- src/dao/ModuloConexao.java
+++ src/dao/ModuloConexao.java
@@ -14,14 +14,17 @@ import java.util.logging.Logger;
  * @author brenocg     
  */
 public class ModuloConexao {
+    public static String userBD = "root";
+    public static String pwBD = "password";
+    
     //metodo por estabelecer a conexao com o banco 
     public static Connection conector(String login, String pw){
         Connection conexao = null;
         // a linha abaixo "chama" o driver que eu instalei(windows importa)
         String driver = "com.mysql.jdbc.Driver";   
         //informacoes do banco                 
-        String url = "jdbc:mysql://localhost:3306/DB-Aula"; //falta o nome do Banco aqui
-        String user = login;//vai mah
+        String url = "jdbc:mysql://localhost:3306/biblioteca?autoReconnect=true&useSSL=false";
+        String user = login;
         String password = pw;
         
         //Estabelecendo comunicacao com o banco
@@ -40,4 +43,28 @@ public class ModuloConexao {
         return conexao;
     }
     
+    public static Connection conector(){
+        Connection conexao = null;
+        // a linha abaixo "chama" o driver que eu instalei(windows importa)
+        String driver = "com.mysql.jdbc.Driver";   
+        //informacoes do banco                 
+        String url = "jdbc:mysql://localhost:3306/biblioteca?autoReconnect=true&useSSL=false";
+        String user = userBD;
+        String password = pwBD;
+        
+        //Estabelecendo comunicacao com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        }
+        
+        catch (Exception e){
+            System.out.println("problema ao conectar ao banco de dados");
+            //e.printStackTrace();
+            System.out.println(e);
+            System.out.println("com usuario " + user + " e senha " + password);
+        }   
+        return conexao;
+    }
 }
