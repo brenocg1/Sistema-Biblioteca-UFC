d994bb003f7a12d3e4c7973b0ba3a7d6b61ba21a
1brenocg <brenocg@alu.ufc.br>
2018-11-15 23:20:09 -0300

    Modelo Aluno e Modelo Telefone

---
+++ src/modelos/Aluno.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author brenocg
+ */
+public class Aluno {
+    ArrayList<telefone> telefones;
+    
+    String nome;
+    String matricula;
+    String endereco;
+    Date dataIngresso;
+    String curso;
+
+    public Aluno(ArrayList<telefone> telefones, String nome, String matricula, String endereco, Date dataIngresso, String curso) {
+        this.telefones = telefones;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.endereco = endereco;
+        this.dataIngresso = dataIngresso;
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + nome + " Matricula: " + matricula;
+    }
+}
---
+++ src/modelos/telefone.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author brenocg
+ */
+public class telefone {
+    String telefone;
+
+    public telefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); 
+    }
+}
