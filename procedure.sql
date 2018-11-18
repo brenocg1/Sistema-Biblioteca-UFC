2b7b9377d6d0ea4f73c3a6067188a9b67901e2a7
1brenocg <brenocg@alu.ufc.br>
2018-11-18 12:19:24 -0300

    Procedure de cadastro de emprestimos e trigguer para validar data de conclusao

---
+++ procedure.sql
@@ -0,0 +1,33 @@
+delimiter $$;
+
+drop procedure pr_cadastra_emprestimo;
+
+create procedure pr_cadastra_emprestimo(var_cod_pessoa int ,var_isbn varchar(13))
+begin
+
+	declare tipo_acesso char(1);
+    declare dt_devolucao date;
+	
+    if exists (select `cod-usuario` from tb_usuario where `tipo-acesso` = 'alun' and `cod-pessoa` = var_cod_pessoa) then
+        set tipo_acesso := 'a';        		  
+    elseif exists (select `cod-usuario` from tb_usuario where `tipo-acesso` = 'func' and `cod-pessoa` = var_cod_pessoa) then
+		set tipo_acesso := 'f';
+	else
+		set tipo_acesso := 'p';
+    end if;
+    
+    if (tipo_acesso = 'a') then
+		set dt_devolucao = date_add(now(), interval 15 day);
+    end if;
+    
+	if (@tipo_acesso = 'f') then
+		set @dt_devolucao = date_add(now(), interval 21 day);
+    end if;
+    
+	if (@tipo_acesso = 'p') then
+		set @dt_devolucao = date_add(now(), interval 30 day);
+    end if;
+    
+	insert into tb_emprestimo (`data-devolucao`, `data-emprestimo` , isbn, `cod-pessoa`) values (@dt_devolucao, now(), var_isbn, var_cod_pessoa);
+
+end $$;
---
+++ trigger_tb_aluno.sql
@@ -0,0 +1,18 @@
+delimiter $$;
+
+drop trigger tr_inc_aluno;
+
+create trigger tr_inc_aluno after insert on tb_aluno
+for each row
+begin
+
+	if exists (select `cod-aluno` from tb_aluno where `data-de-conclusao-prevista` <  cast(now() as date)) then
+
+		signal sqlstate '45000' set message_text = 'Insercao nao permitida!';
+
+	end if;
+
+
+end $$;
+
+
